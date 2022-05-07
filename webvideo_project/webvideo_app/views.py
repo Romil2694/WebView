3f3456def0b1ca6aae398ea1c2afd59dcf957f69
Romil2694 <36949554+Romil2694@users.noreply.github.com>
2022-05-07 12:48:51 +0530

    Add files via upload

--- webvideo_project/webvideo_app/views.py
+++ webvideo_project/webvideo_app/views.py
@@ -3,19 +3,7 @@ from django.http import StreamingHttpResponse
 from .camera import videocapture
 
 
-# Create your views here.
+# Create your views here
 
-def index(request):
-    return render(request, './index.html')
-
-
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-
-def video_stream(request):
-    return StreamingHttpResponse(gen(videocapture()),
-                                 content_type='multipart/x-mixed-replace; boundary=frame')
+def webvideo(request):
+    return render(request, './webvideo.html')
