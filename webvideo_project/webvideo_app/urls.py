0da2e11a3352297b899dffe82ae734b806571f47
Romil2694 <36949554+Romil2694@users.noreply.github.com>
2022-05-07 12:49:30 +0530

    Add files via upload

--- webvideo_project/webvideo_app/urls.py
+++ webvideo_project/webvideo_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('video_stream/', views.video_stream, name='video_stream')
+    path('video_stream/', views.webvideo, name='video_stream')
 ]
